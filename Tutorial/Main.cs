9705a63c18aa542e269199ff2472fd163f905610
itshdog <haydenwestphal@gmail.com>
2022-06-06 18:39:10 -0500

    More main code commits

--- Tutorial/Main.cs
+++ Tutorial/Main.cs
@@ -2,68 +2,101 @@
 using BepInEx.Configuration;
 using R2API;
 using R2API.Utils;
+using RoR2;
 using System;
 using System.Reflection;
+using System.IO;
 using Tutorial.Base_Classes;
 using UnityEngine;
 
+public static class ConfigManager
+{
+    private static ConfigFile configFileA;
+    private static ConfigFile configFileB;
+    private const string configFolderName = "myConfigFolder";
+    private static string ConfigFolderPath { get => System.IO.Path.Combine(Paths.ConfigPath, configFolderName); }
 
+    private static void Init()
+    {
+        // Ensures that the config folder exists, if not, create it.
+        if (!Directory.Exists(ConfigFolderPath))
+        {
+            Directory.CreateDirectory(ConfigFolderPath);
+        }
+        //Create the config files, notice how we use Path.Combine for creating the new files.            
+        configFileA = new ConfigFile(System.IO.Path.Combine(ConfigFolderPath, "ConfigFileA.CFG"), true);
+        configFileB = new ConfigFile(System.IO.Path.Combine(ConfigFolderPath, "ConfigFileB.CFG"), true);
+    }
+}
 
-//automatically renamed based on project name.
 namespace Tutorial
 {
-    //--------------R2API dependency. This template is heavily based on the modules provided by this API, so it uses it as a dependency.--------------------
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
-
-    //add other mod dependencies here using the format listed above.
-
-    //------------------Makes mod server-side. Change this only if you're sure it won't fuck something up-----------------------
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
-
-    //------------------Defines your mod's GUID, Name, and Version to BepIn. These are set in variables later.---------------------------
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-
-    //------------------This line adds R2API dependencies required. ONLY ADD THE ONES YOU NEED. If you need to know which ones you need, check the R2API documentation----------
     [R2APISubmoduleDependency(nameof(ResourcesAPI), nameof(LanguageAPI), nameof(UnlockableAPI), nameof(ItemAPI))]
 
-    //main class where the most basic stuff happens. Basically only here to activate the mod.
     public class Main : BaseUnityPlugin
     {
 
-        //define mod ID: uses the format of "com.USERNAME.MODNAME"
+        //"com.USERNAME.MODNAME"
         public const string ModGUID = "com.itshdog.tutorial";
-
-        //define the mod name inside quotes. Can be anything.
         public const string ModName = "itshdog Tutorial";
-
-        //define mod version inside quotes. Follows format of "MAJORVERSION.MINORPATCH.BUGFIX". Ex: 1.2.3 is Major Release 1, Patch 2, Bug Fix 3.
+        //"MAJORVERSION.MINORPATCH.BUGFIX". Ex: 1.2.3 is Major Release 1, Patch 2, Bug Fix 3.
         public const string ModVersion = "0.0.1";
-
-        //Creates an asset bundle that can be easily accessed from other classes
         public static AssetBundle Assets;
+        public static ConfigEntry<string> CommandoSlideMessage { get; set; }
+        public static ConfigEntry<string> CommandoShootMessage { get; set; }
 
-        //List other necessary variables and bits here. For example, you may need a list of all your new things to add them to the game properly.
-
-        //this method runs when your mod is loaded.
         public void Awake()
         {
+            // Initial Load Message
+            Chat.AddMessage("Mr.Sarcasm v0.0.1 Loaded!");
+
+            // Config
+            CommandoSlideMessage = Config.Bind<string>(
+                "Commando",
+                "SlideMessage",
+                "*Gracefully slides ass across the ground*",
+                "This is the message which is played when SlideState.OnEnter is called"
+            );
+
+            CommandoShootMessage = Config.Bind<string>(
+                "Commando",
+                "ShootMessage",
+                "Pew! Pew! Pew!",
+                "This is the message which is played when FirePistol2.OnEnter is called"
+            );
+
+            // Main
+            var rand = new System.Random();
 
             On.EntityStates.Commando.SlideState.OnEnter += (orig, self) =>
             {
-                ChatMessage.Send("[You]: *Gracefully slides ass across the ground*");
-                orig(self);
+                int chance = rand.Next(1, 101);
+
+                if (chance <= 25)
+                {
+                    ChatMessage.SendColored(CommandoSlideMessage.Value, Color.gray);
+                    orig(self);
+                } else
+                {
+                    orig(self);
+                }
             };
 
             On.EntityStates.Commando.CommandoWeapon.FirePistol2.OnEnter += (orig, self) =>
             {
-                ChatMessage.Send("[You]: Pew!");
-                orig(self);
-            };
+                int chance = rand.Next(1, 101);
 
-            On.EntityStates.Commando.CommandoWeapon.FireShotgunBlast.OnEnter += (orig, self) =>
-            {
-                ChatMessage.Send("[You]: Big Pew!");
-                orig(self);
+                if (chance <= 5)
+                {
+                    ChatMessage.SendColored(CommandoShootMessage.Value, Color.grey);
+                    orig(self);
+                } else
+                {
+                    orig(self);
+                }
             };
 
             //loads an asset bundle if one exists. Objects will need to be called from this bundle using AssetBundle.LoadAsset(string path)
@@ -87,6 +120,16 @@ namespace Tutorial
 
             //runs hooks that are seperate from all additions (i.e, if you need to call something when the game runs or at special times)
             Hooks();
+        }
+
+        private void PickupDropletController_CreatePickupDroplet(On.RoR2.PickupDropletController.orig_CreatePickupDroplet orig, PickupIndex pickupIndex, UnityEngine.Vector3 position, UnityEngine.Vector3 velocity)
+        {
+            if (pickupIndex == PickupCatalog.FindPickupIndex(RoR2Content.Items.Hoof.itemIndex))
+            {
+                Chat.AddMessage("Goat lookin' ass LMAOO");
+                ChatMessage.Send("GOTTA RUN FAST!");
+            }
+            orig(pickupIndex, position, velocity);
         }
 
         private void FirePistol2_FireBullet(On.EntityStates.Commando.CommandoWeapon.FirePistol2.orig_FireBullet orig, EntityStates.Commando.CommandoWeapon.FirePistol2 self, string targetMuzzle)
@@ -147,11 +190,6 @@ namespace Tutorial
                 achievement.Init(base.Config);
 
             }
-
         }
-
-        //place other necessary methods below
-
     }
-
 }
